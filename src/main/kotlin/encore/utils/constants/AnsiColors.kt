3007601cfaed2d8663a972cb5c419a3c26f606f3
glennhenry <glennhenrybdg05@gmail.com>
2026-03-18 21:35:00 +0700

    Update AnsiColors.kt

--- src/main/kotlin/encore/utils/constants/AnsiColors.kt
+++ src/main/kotlin/encore/utils/constants/AnsiColors.kt
@@ -2,6 +2,8 @@ package encore.utils.constants
 
 /**
  * Ansi colors (256) constants to style console.
+ *
+ * Must use "--console=plain" for color to work consistently.
  */
 @Suppress("unused", "ConstPropertyName")
 object AnsiColors {
@@ -9,22 +11,18 @@ object AnsiColors {
     const val Reset = "$Esc[0m"
 
     const val BlackText = "$Esc[38;5;16m"
-    const val BlackBg = "$Esc[48;5;16m"
 
-    const val WhiteText = "$Esc[38;5;255m"
-    const val WhiteBg = "$Esc[48;5;255m"
+    const val TraceFg = "$Esc[38;5;66m"  // #5F8787 muted teal
+    const val DebugFg = "$Esc[38;5;219m" // #FFAFFF bubble gum pink
+    const val InfoFg = "$Esc[38;5;153m"  // #AFD7FF sky blue
+    const val WarnFg = "$Esc[38;5;221m"  // #FFD75F dandelion
+    const val ErrorFg = "$Esc[38;5;203m" // #FF5F5F pastel red
 
-    const val VerboseFg = "$Esc[38;5;66m"
-    const val DebugFg = "$Esc[38;5;219m"
-    const val InfoFg = "$Esc[38;5;153m"
-    const val WarnFg = "$Esc[38;5;221m"
-    const val ErrorFg = "$Esc[38;5;203m"
-
-    const val VerboseBg = "$Esc[48;5;66m"
-    const val DebugBg = "$Esc[48;5;219m"
-    const val InfoBg = "$Esc[48;5;153m"
-    const val WarnBg = "$Esc[48;5;221m"
-    const val ErrorBg = "$Esc[48;5;203m"
+    const val TraceBg = "$Esc[48;5;66m"  // #5F8787 muted teal
+    const val DebugBg = "$Esc[48;5;219m" // #FFAFFF bubble gum pink
+    const val InfoBg = "$Esc[48;5;153m"  // #AFD7FF sky blue
+    const val WarnBg = "$Esc[48;5;221m"  // #FFD75F dandelion
+    const val ErrorBg = "$Esc[48;5;203m" // #FF5F5F pastel red
 
     fun fg(n: Int) = "$Esc[38;5;${n}m"
     fun bg(n: Int) = "$Esc[48;5;${n}m"
