a4c82422d1adca830529fcc93267dd7676a6cd4d
glennhenry <glennhenrybdg05@gmail.com>
2026-03-26 14:34:31 +0700

    Split into two file

--- src/main/kotlin/encore/fancam/producer/FancamProducer.kt
+++ src/main/kotlin/encore/fancam/producer/FancamProducer.kt
@@ -1,15 +1,7 @@
 package encore.fancam.producer
 
-import encore.EncoreFancamConfig
-import encore.fancam.LOG_FILE_DIRECTORY
-import encore.fancam.events.FileRoutableEvent
 import encore.fancam.events.LogEvent
 import encore.fancam.events.TrackEvent
-import encore.fancam.formatter.FancamFormatter
-import encore.fancam.formatter.LogEventFancamFormatter
-import encore.fancam.formatter.TrackEventFancamFormatter
-import encore.utils.toMB
-import encore.utils.getRotatedFile
 
 /**
  * Represent a producer for log or track event.
@@ -25,35 +17,4 @@ import encore.utils.getRotatedFile
  */
 interface FancamProducer<T> {
     fun produce(event: T)
-}
-
-/**
- * Producer implementation tailored for file output.
- *
- * A formatter is expected for this implementation, which may be
- * - [LogEventFancamFormatter]
- * - [TrackEventFancamFormatter]
- *
- * Produce is implemented by appending the result string of the formatter
- * into the event's target file with rotation enabled.
- */
-class FileFancamProducer<T>(
-    private val config: EncoreFancamConfig,
-    private val formatter: FancamFormatter<T>
-) : FancamProducer<T> where T : FileRoutableEvent {
-    override fun produce(event: T) {
-        val filename = event.filename ?: return
-
-        val file = getRotatedFile(
-            directory = LOG_FILE_DIRECTORY,
-            filename = filename,
-            extension = event.extension,
-            maxRotation = config.maxFileRotation
-        ) { file ->
-            file.length() > (config.maxFileSize.toMB())
-        }
-
-        val text = formatter.format(event)
-        file.appendText("$text\n")
-    }
 }
