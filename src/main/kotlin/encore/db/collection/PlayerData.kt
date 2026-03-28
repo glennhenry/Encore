891a9448610de01362b503287dccb57313a44087
glennhenry <glennhenrybdg05@gmail.com>
2026-03-28 16:49:55 +0700

    Delete GameData

--- src/main/kotlin/encore/db/collection/PlayerData.kt
+++ src/main/kotlin/encore/db/collection/PlayerData.kt
@@ -1,27 +1,23 @@
 package encore.db.collection
 
 import encore.user.AdminData
-import encore.core.model.GameData
 
 /**
  * Database-level representation of a player's game data.
  */
 data class PlayerData(
     val playerId: String,
-    val data: GameData
 ) {
     companion object {
         fun admin(): PlayerData {
             return PlayerData(
                 playerId = AdminData.PLAYER_ID,
-                data = GameData.admin()
             )
         }
 
         fun newGame(playerId: String): PlayerData {
             return PlayerData(
                 playerId = playerId,
-                data = GameData.newGame()
             )
         }
     }
