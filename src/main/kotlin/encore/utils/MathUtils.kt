cd3b159bca51ffd75614382ea7599340568b43ba
glennhenry <glennhenrybdg05@gmail.com>
2026-04-07 14:32:44 +0700

    Create hashing utility

--- src/main/kotlin/encore/utils/MathUtils.kt
+++ src/main/kotlin/encore/utils/MathUtils.kt
@@ -1,3 +1,16 @@
 package encore.utils
 
+import com.toxicbakery.bcrypt.Bcrypt
+import kotlin.io.encoding.Base64
+
 fun Int.toMB(): Long = this * 1024L * 1024L
+
+/**
+ * Hash the given [s] string using the Bcrypt function
+ * with the specified salt [rounds].
+ *
+ * @return Hashed string in Base64 representation.
+ */
+fun hash(s: String, rounds: Int = 10): String {
+    return Base64.encode(Bcrypt.hash(s, rounds))
+}
