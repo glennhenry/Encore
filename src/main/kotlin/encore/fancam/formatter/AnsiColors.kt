1b4897f80009ad657215fe481f60d9a26f1a6ffc
glennhenry <glennhenrybdg05@gmail.com>
2026-04-09 15:30:58 +0700

    Improve foreground color readability and add color for tag

--- src/main/kotlin/encore/fancam/formatter/AnsiColors.kt
+++ src/main/kotlin/encore/fancam/formatter/AnsiColors.kt
@@ -12,17 +12,19 @@ object AnsiColors {
 
     const val BlackText = "$Esc[38;5;16m"
 
-    const val TraceFg = "$Esc[38;5;66m"  // #5F8787 muted teal
-    const val DebugFg = "$Esc[38;5;219m" // #FFAFFF bubble gum pink
-    const val InfoFg = "$Esc[38;5;153m"  // #AFD7FF sky blue
-    const val WarnFg = "$Esc[38;5;221m"  // #FFD75F dandelion
-    const val ErrorFg = "$Esc[38;5;203m" // #FF5F5F pastel red
+    const val TraceFg = "$Esc[38;5;29m"  // #00875F emerald green
+    const val DebugFg = "$Esc[38;5;13m"  // #FF00FF fuchsia
+    const val InfoFg = "$Esc[38;5;19m"   // #0000AF duke blue
+    const val WarnFg = "$Esc[38;5;178m"  // #D7AF00 golden rod
+    const val ErrorFg = "$Esc[38;5;196m" // #FF0000 bright red
+    const val TagFg = "$Esc[38;5;54m"    // #5F0087 metallic violet
 
     const val TraceBg = "$Esc[48;5;66m"  // #5F8787 muted teal
     const val DebugBg = "$Esc[48;5;219m" // #FFAFFF bubble gum pink
     const val InfoBg = "$Esc[48;5;153m"  // #AFD7FF sky blue
     const val WarnBg = "$Esc[48;5;221m"  // #FFD75F dandelion
     const val ErrorBg = "$Esc[48;5;203m" // #FF5F5F pastel red
+    const val TagBg = "$Esc[48;5;254m"   // #E4E4E4 platinum
 
     fun fg(n: Int) = "$Esc[38;5;${n}m"
     fun bg(n: Int) = "$Esc[48;5;${n}m"
