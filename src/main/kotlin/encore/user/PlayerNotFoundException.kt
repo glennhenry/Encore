1d6ada4dbfc3d227d3c817b500c5459157d1d2de
glennhenry <glennhenrybdg05@gmail.com>
2026-04-15 22:22:32 +0700

    Delete unused PlayerNotFoundException.kt

--- src/main/kotlin/encore/user/PlayerNotFoundException.kt
+++
@@ -1,8 +0,0 @@
-package encore.user
-
-import encore.datastore.collection.PlayerId
-
-/**
- * Thrown when a MongoDB query expected a document from [playerId] but found none.
- */
-class PlayerNotFoundException(playerId: PlayerId) : RuntimeException("Player not found: $playerId")
