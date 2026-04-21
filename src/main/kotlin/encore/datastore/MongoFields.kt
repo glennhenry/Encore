7e7504c5c2b831a2d0e180782396fe5b6495849a
glennhenry <glennhenrybdg05@gmail.com>
2026-04-21 21:58:05 +0700

    Add comments for quick preview on field name

--- src/main/kotlin/encore/datastore/MongoFields.kt
+++ src/main/kotlin/encore/datastore/MongoFields.kt
@@ -10,13 +10,32 @@ import org.bson.conversions.Bson
 // This file contains constants of Kotlin's data class fields' name
 // that is used in Mongo queries.
 
-// this is same for most playerId fields
+/** `playerId`*/
 val FieldPlayerId = PlayerAccount::playerId.name
+
+/** `username`*/
 val FieldUsername = PlayerAccount::username.name
+
+/** `email`*/
 val FieldEmail = PlayerAccount::email.name
+
+/** `password`*/
 val FieldPassword = PlayerAccount::hashedPassword.name
+
+/** `profile`*/
 val FieldProfile = PlayerAccount::profile.name
+
+/** `profile.lastActiveAt`*/
 val FieldProfileLastActive = "$FieldProfile.${Profile::lastActiveAt.name}"
 
+/** `acts`*/
+val FieldActs = ServerObjects::acts.name
+
+/** `acts.playerId`*/
+val FieldActsPlayerId = "$FieldActs.$FieldPlayerId"
+
+/** `dbId`*/
 val ServerObjectsDbId = ServerObjects::dbId.name
+
+/** Mongo filters for `dbId` equals [ServerObjectsId]*/
 val ServerObjectsFilter: Bson = Filters.eq(ServerObjectsDbId, ServerObjectsId)
