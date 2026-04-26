e6ec9690f40848bb09d42b3b183d3e646cc52938
glennhenry <glennhenrybdg05@gmail.com>
2026-04-26 21:53:42 +0700

    Change DefaultHandler expectation to DefaultMessage

--- src/main/kotlin/encore/network/handler/DefaultHandler.kt
+++ src/main/kotlin/encore/network/handler/DefaultHandler.kt
@@ -1,20 +1,20 @@
 package encore.network.handler
 
 import encore.fancam.Fancam
-import encore.network.messaging.socket.SocketMessage
+import encore.network.messaging.socket.DefaultMessage
 import kotlin.reflect.KClass
 
 /**
  * Default handler as the fallback for any unregistered socket handlers.
  */
-class DefaultHandler : SocketMessageHandler<SocketMessage> {
+class DefaultHandler : SocketMessageHandler<DefaultMessage> {
     override val name: String = "DefaultHandler"
     override val messageType: String = "Default"
-    override val expectedMessageClass: KClass<SocketMessage> = SocketMessage::class
+    override val expectedMessageClass: KClass<DefaultMessage> = DefaultMessage::class
 
-    override fun shouldHandle(message: SocketMessage): Boolean = true
+    override fun shouldHandle(message: DefaultMessage): Boolean = true
 
-    override suspend fun handle(ctx: HandlerContext<SocketMessage>) = with(ctx) {
+    override suspend fun handle(ctx: HandlerContext<DefaultMessage>) = with(ctx) {
         Fancam.warn { "No handler registered/implemented for type=${message.type()}" }
     }
 }
