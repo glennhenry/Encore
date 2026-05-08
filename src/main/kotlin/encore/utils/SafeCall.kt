fb45b9285f3423981507b3708fb8012c08983295
glennhenry <glennhenrybdg05@gmail.com>
2026-05-08 21:59:55 +0700

    Create SafeCall.kt

---
+++ src/main/kotlin/encore/utils/SafeCall.kt
@@ -0,0 +1,39 @@
+package encore.utils
+
+import encore.fancam.Fancam
+
+/**
+ * Safely execute the given [block] within a try-catch guard.
+ * This will log any caught [Throwable] via [Fancam] and
+ * returns `null` if an error occurs.
+ *
+ * @param block The block of code to run.
+ * @param R The return type of block.
+ * @return The result of [block] or `null` if an exception was thrown.
+ */
+fun <R> safely(block: () -> R): R? {
+    return try {
+        block()
+    } catch (t: Throwable) {
+        Fancam.error(t) { "Error caught on safely" }
+        null
+    }
+}
+
+/**
+ * Safely execute the given [block] within a try-catch guard.
+ * This will log any caught [Throwable] via [Fancam] and
+ * returns `null` if an error occurs.
+ *
+ * @param block The block of code to run.
+ * @param R The return type of block.
+ * @return The result of [block] or `null` if an exception was thrown.
+ */
+suspend fun <R> safelySuspend(block: suspend () -> R): R? {
+    return try {
+        block()
+    } catch (t: Throwable) {
+        Fancam.error(t) { "Error caught on safelySuspend" }
+        null
+    }
+}
