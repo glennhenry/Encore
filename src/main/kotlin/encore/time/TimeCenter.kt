f1b268c7c61185cd93c3bb740391d80fa05434c0
glennhenry <glennhenrybdg05@gmail.com>
2026-05-21 22:45:54 +0700

    Use default for timekeeper to avoid must init error

--- src/main/kotlin/encore/time/TimeCenter.kt
+++ src/main/kotlin/encore/time/TimeCenter.kt
@@ -1,6 +1,7 @@
 package encore.time
 
 import encore.fancam.Fancam
+import encore.time.source.SystemTimeSource
 
 /**
  * Central application component that provides access to [system] and [game]
@@ -13,34 +14,35 @@ import encore.fancam.Fancam
  * For instance, utilizing a real system time for system components, while
  * leveraging a sped up timing model for game mechanics.
  *
- * Should call [initialize] before usage.
+ * By default, [TimeCenter] set [system] and [game] with a [Timekeeper]
+ * supplied with [SystemTimeSource]. This avoids the annoying must init on usage.
+ * The [update] can be used to modify the internal timekeeper.
  */
 object TimeCenter {
-    private var _system: Timekeeper? = null
+    private var initialized = false
+    private var _system: Timekeeper = Timekeeper(SystemTimeSource())
+    private var _game: Timekeeper = Timekeeper(SystemTimeSource())
 
     /**
      * The [Timekeeper] used for system components (e.g., authentication, DB, system timer, etc.).
      */
-    val system: Timekeeper
-        get() = _system ?: error("System timekeeper is not initialized. Call TimeCenter.initialize() first.")
-
-    private var _game: Timekeeper? = null
+    val system: Timekeeper = _system
 
     /**
      * The [Timekeeper] used for game components (e.g., game timer, ticks, daily updates, etc.).
      */
-    val game: Timekeeper
-        get() = _game ?: error("Game timekeeper is not initialized. Call TimeCenter.initialize() first.")
+    val game: Timekeeper = _game
 
     /**
      * Initialize [TimeCenter] with [Timekeeper] instance for system and game.
      */
-    fun initialize(system: Timekeeper, game: Timekeeper) {
-        if (_system != null) {
-            Fancam.warn { "TimeCenter.initialize() called after initialization. Ignoring." }
+    fun update(system: Timekeeper, game: Timekeeper) {
+        if (initialized) {
+            Fancam.warn { "TimeCenter was already initialized, ignoring." }
             return
         }
         this._system = system
         this._game = game
+        initialized = true
     }
 }
